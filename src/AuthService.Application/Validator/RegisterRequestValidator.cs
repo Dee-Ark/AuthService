d8b1ce3d9e3a8ef31f5301feee8e26b441da74e2
Dee-Ark <@@Damila403200!!>
2025-08-17 20:32:34 +0100

    validate password

--- src/AuthService.Application/Validator/RegisterRequestValidator.cs
+++ src/AuthService.Application/Validator/RegisterRequestValidator.cs
@@ -13,10 +13,15 @@ namespace AuthService.Application.Validator
 
             RuleFor(x => x.Password)
                 .NotEmpty().WithMessage("Password is required.")
-                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
+                .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                .Matches(@"\d").WithMessage("Password must contain at least one number.")
+                .Matches(@"[^\da-zA-Z]").WithMessage("Password must contain at least one special character.");
 
             RuleFor(x => x.FullName)
                 .NotEmpty().WithMessage("FullName is required.")
+                .Matches("^[a-zA-Z ]+$").WithMessage("Full name must contain only letters and spaces.")
                 .MaximumLength(100).WithMessage("Full name must not exceed 100 characters.");
         }
     }
